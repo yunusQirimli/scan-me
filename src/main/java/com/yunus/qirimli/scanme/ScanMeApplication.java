817cea762352b9ebe1ac8234cf203a62b93854b9
Yunus Yunusov <yunus1699@gmail.com>
2022-01-31 18:17:01 +0200

    initialize project

---
+++ src/main/java/com/yunus/qirimli/scanme/ScanMeApplication.java
@@ -0,0 +1,13 @@
+package com.yunus.qirimli.scanme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScanMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScanMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yunus/qirimli/scanme/ScanMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunus.qirimli.scanme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScanMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
