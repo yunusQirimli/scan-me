ff9186629fdf15057d7585effb6418a86abf86eb
Yunus Yunusov <yunus1699@gmail.com>
2022-02-09 13:32:03 +0200

    [#1] add entities

---
+++ src/main/java/com/yunus/qirimli/scanme/entity/Profile.java
@@ -0,0 +1,68 @@
+package com.yunus.qirimli.scanme.entity;
+
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString(exclude = {"profileOwner", "phones", "contacts", "services"})
+public class Profile {
+
+  @Id
+  @SequenceGenerator(
+      name = "profile_sequence",
+      sequenceName = "profile_sequence",
+      allocationSize = 1)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "profile_sequence")
+  private Long profileId;
+
+  private String title;
+  private String description;
+  private String profilePicture;
+  private boolean active;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinTable(
+      name = "profile_application_user",
+      joinColumns = @JoinColumn(name = "profile_id", referencedColumnName = "profileId"),
+      inverseJoinColumns = @JoinColumn(name = "user_id", referencedColumnName = "userId"))
+  private User profileOwner;
+
+  @ManyToMany(fetch = FetchType.LAZY)
+  @JoinTable(
+      name = "profile_phone",
+      joinColumns = @JoinColumn(name = "profile_id", referencedColumnName = "profileId"),
+      inverseJoinColumns = @JoinColumn(name = "phone_id", referencedColumnName = "phoneId"))
+  private List<Phone> phones;
+
+  @ManyToMany(fetch = FetchType.LAZY)
+  @JoinTable(
+      name = "profile_contact",
+      joinColumns = @JoinColumn(name = "profile_id", referencedColumnName = "profileId"),
+      inverseJoinColumns = @JoinColumn(name = "contact_id", referencedColumnName = "contactId"))
+  private List<Contact> contacts;
+
+  @ManyToMany(fetch = FetchType.LAZY)
+  @JoinTable(
+      name = "profile_service",
+      joinColumns = @JoinColumn(name = "profile_id", referencedColumnName = "profileId"),
+      inverseJoinColumns = @JoinColumn(name = "service_id", referencedColumnName = "serviceId"))
+  private List<Service> services;
+}
